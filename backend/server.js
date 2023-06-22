295b578c7fd2eecfc20723646d8476ab1558d4b4
maitreyaawad10 <maitreya.awad@gmail.com>
2023-06-22 11:12:15 +0530

    added cors import in server.js

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+const cors = require("cors");
 const path = require("path")
 
 
